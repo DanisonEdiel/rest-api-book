6d986e84790059887807b7e2bb79b4a1cea3e45a
Agustin <toapantaagustin9c@gmail.com>
2024-05-05 19:56:26 -0500

    Changes line 18 controller

--- src/main/java/com/uce/RestApiBook/Controller/BookController.java
+++ src/main/java/com/uce/RestApiBook/Controller/BookController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 @CrossOrigin(origins = {"https://react-book-frontend-production.up.railway.app/","http://localhost:3000/"})
 @RestController
 @RequestMapping("/api")
-@Tag(name = "Api Rest for books use Swagger 3")
+@Tag(name = "Api Rest for books use Swagger 3 - Books")
 public class BookController {
     @Autowired
     private BookRepository bookRepository;
