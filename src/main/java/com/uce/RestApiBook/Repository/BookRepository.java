c836cacb9c6a811b9be2059a2e177d682929ef7f
danisonediel <edison.shano@hotmail.com>
2024-05-15 17:52:10 -0500

    Changes

--- src/main/java/com/uce/RestApiBook/Repository/BookRepository.java
+++ src/main/java/com/uce/RestApiBook/Repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.uce.RestApiBook.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.uce.RestApiBook.Entity.Book;
 
